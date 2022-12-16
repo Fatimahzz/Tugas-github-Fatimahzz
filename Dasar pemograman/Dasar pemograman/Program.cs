dcb45319d6580357c8d020ae0086b57aee805d03
Fatimahzz <120366476+Fatimahzz@users.noreply.github.com>
2022-12-16 18:56:47 +0700

    Add files via upload

---
+++ Dasar pemograman/Dasar pemograman/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Anda adalah agen rahasia yang bertugas mendapatkan data dan server...");
+Console.WriteLine("Akses ke server membutuhkan password yang tidak diketahui");
